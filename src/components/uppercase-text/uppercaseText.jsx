3b8ffb70726ee311e5cbb9a6eb2ef1d3644b7356
danielveg4 <dvgilles@gmail.com>
2024-09-04 11:00:40 +0200

    del

--- src/components/uppercase-text/uppercaseText.jsx
+++
@@ -1,7 +0,0 @@
-/* Crea un componente que reciba una cadena de texto, la convierta a mayúsculas, y devuelva un h2 con el texto en mayúsculas. */
-
-const UppercaseText = ({ text }) => {
-	return <h2>{text.toUpperCase()}</h2>;
-};
-
-export default UppercaseText;
